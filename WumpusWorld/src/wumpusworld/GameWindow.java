d4e2453b1b6a44f6c6a2e45f08332f60ef4fbc39
l.raczkiewiczleszekp <l.raczkiewiczleszekp@FR-LL2706959.groupinfra.com>
2017-12-14 14:07:37 +0100

    [Pavel][Board.java] j'ai degagé les commentaires innutiles lol

--- WumpusWorld/src/wumpusworld/GameWindow.java
+++ WumpusWorld/src/wumpusworld/GameWindow.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wumpusworld;
 
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-/**
- *
- * @author frus71323
- */
 public class GameWindow {
     
     private int windowsSize = 800;
--- WumpusWorld/src/wumpusworld/WumpusWorld.java
+++ WumpusWorld/src/wumpusworld/WumpusWorld.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package wumpusworld;
 
 import javax.swing.JFrame;
 
-/**
- *
- * @author frus71323
- */
 public class WumpusWorld {
 
-    /**
-     * @param args the command line arguments
-     */
+
     private static final int numberOfCells = 9;
 
     public static void main(String[] args) {
