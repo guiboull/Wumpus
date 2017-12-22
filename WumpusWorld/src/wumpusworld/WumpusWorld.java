4275251824ce865d1a5393eab157a8f1ee3cad86
l.raczkiewiczleszekp <l.raczkiewiczleszekp@FR-LL2706959.groupinfra.com>
2017-12-22 08:35:19 +0100

    [Pavel][ShortestPath.java] Ajout d'un debut  de Djikstra

--- WumpusWorld/src/wumpusworld/WumpusWorld.java
+++ WumpusWorld/src/wumpusworld/WumpusWorld.java
@@ -10,13 +10,18 @@ public class WumpusWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         Board boardGame = new Board(numberOfCells, numberOfCells);
-        boardGame.setBoard(2);
+        boardGame.setBoard(5);
         boardGame.showBoard();
-        boardGame.showBoard2();
-        boardGame.showBoard3();
+        //boardGame.showBoard2();
+        //boardGame.showBoard3();
         GameWindow mWindow= new GameWindow(boardGame);
         //boardGame.cheminPasTropOpti(numberOfCells, 1, Direction.N, "", 20);
         //System.out.println(boardGame.getCheminPasTropOpti());
+        boardGame.getProxi(1,9);
+        
+        ShortestPath d = new ShortestPath(numberOfCells, numberOfCells, boardGame.colGold, boardGame.rowGold, boardGame.proxiBoard);
+        d.djikstra();
+        d.showDjikstra();
     }  
     
 }
