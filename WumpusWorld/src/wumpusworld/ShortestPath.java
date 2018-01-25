762765186b1b961fb50c95f850ffbfd7c2c1176b
l.raczkiewiczleszekp <l.raczkiewiczleszekp@FR-LL2706959.groupinfra.com>
2018-01-25 12:23:51 +0100

    [Pavel][ShortestPath.java] Ajout d'un debut  de Djikstra

--- WumpusWorld/src/wumpusworld/ShortestPath.java
+++ WumpusWorld/src/wumpusworld/ShortestPath.java
@@ -11,6 +11,8 @@ public class ShortestPath {
     protected int[][] djikstra;
     protected ArrayList<int[]> nodeList;
     protected boolean[][][][] proxi;
+    protected ArrayList<Duo> lastNode;
+    protected ArrayList<int[]> path;
 
     public ShortestPath(int col, int row, int colGold, int rowGold, boolean[][][][] proxi) {
         col = col + 2;
@@ -20,6 +22,8 @@ public class ShortestPath {
         this.colGold = colGold;
         this.rowGold = rowGold;
         nodeList = new ArrayList<int[]>();
+        lastNode = new ArrayList<Duo>();
+        path = new ArrayList<>();
 
         djikstra = new int[col][row];
         for (int i = 0; i < col; i++) {
@@ -30,6 +34,7 @@ public class ShortestPath {
                 node[1] = j;
                 if (i != 0 && j != 0 && i != col - 1 && j != row - 1) {
                     nodeList.add(node);
+                    lastNode.add(new Duo(node, node));
                 }
             }
         }
@@ -38,21 +43,15 @@ public class ShortestPath {
     }
 
     public void djikstra() {
+        System.out.println("DJIKSTRA");
         int[] mem = new int[2];
         mem[0] = 9;
         mem[1] = 1;
         nodeList.remove(mem);
-        showDjikstra();
-
-        for (int i = 0; i < nodeList.size(); i++) {
-            int[] test = nodeList.get(i);
-            System.out.print("[" + test[0] + "," + test[1] + "]");
-        }
-
+        
         while (djikstra[colGold][rowGold] == 10000) {
             mem = findMin();
             nodeList.remove(mem);
-            showDjikstra();
             if (proxi[mem[0]][mem[1]][mem[0] - 1][mem[1]]) {
                 int[] mem2 = new int[2];
                 mem2[0] = mem[0] - 1;
@@ -91,6 +90,7 @@ public class ShortestPath {
     }
 
     public void showDjikstra() {
+        System.out.println(colGold + " " + rowGold);
         for (int i = 0; i < col; i++) {
             for (int j = 0; j < row; j++) {
                 if (djikstra[i][j] == 10000) {
@@ -101,6 +101,16 @@ public class ShortestPath {
             }
             System.out.println();
         }
+        System.out.println("PATH");
+        System.out.println(path.size());
+        path();
+        System.out.println("PATH OK");
+        System.out.println(path.size());
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print("[" + path.get(i)[0] + "," + path.get(i)[1] + "]");
+        }
+        System.out.print("[" + (col-2) + "," + 1 + "]");
+        System.out.println("DJIKSTRA OK");
     }
 
     public int[] findMin() {
@@ -135,7 +145,6 @@ public class ShortestPath {
                 mem[1] = nRow - 1;
             }
         }
-        System.out.println(mem[0] + " " + mem[1]);
         return mem;
     }
 
@@ -146,11 +155,45 @@ public class ShortestPath {
     public void updateDist(int[] node1, int[] node2) {
         if (djikstra[node2[0]][node2[1]] > djikstra[node1[0]][node1[1]] + 1) {
             djikstra[node2[0]][node2[1]] = djikstra[node1[0]][node1[1]] + 1;
+            for (int i = 0; i < lastNode.size(); i++) {
+                if (lastNode.get(i).getFirst()[0] == node2[0] && lastNode.get(i).getFirst()[1] == node2[1]) {
+                    lastNode.get(i).setSecond(node1);
+                }
+            }
         }
     }
 
+    public void path() {
+        int itX = colGold;
+        int itY = rowGold;
+
+        while (itX != col - 2 || itY != 1) {
+            int tempItX = -1;
+            int tempItY = -1;
+            int[] node = new int[2];
+            node[0] = itX;
+            node[1] = itY;
+            path.add(node);
+            for (int i = 0; i < lastNode.size(); i++) {
+                if (lastNode.get(i).getFirst()[0] == itX && lastNode.get(i).getFirst()[1] == itY) {
+                    tempItX = lastNode.get(i).getSecond()[0];
+                    tempItY = lastNode.get(i).getSecond()[1];
+                }
+            }
+            itX = tempItX;
+            itY = tempItY;
+        }
+    }
 }
 /*
+
+A = suite vide
+2 s := sfin
+3 tant que s != sdeb faire
+4   A = A + s                  on ajoute s à la suite A 
+5   s = prédécesseur[s]        on continue de suivre le chemin 
+6 fin tant que
+
 Dijkstra(G,Poids,sdeb)
 1 Initialisation(G,sdeb)
 2 Q := ensemble de tous les nœuds
