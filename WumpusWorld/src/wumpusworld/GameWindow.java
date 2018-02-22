eb8619b107ae7c9505357282fb6a6965127cf7f5
frus71323 <frus71323@GFI541560.annuaire.groupe.local>
2018-02-22 11:47:16 +0100

    Change Graphic Interface

--- WumpusWorld/src/wumpusworld/GameWindow.java
+++ WumpusWorld/src/wumpusworld/GameWindow.java
@@ -2,7 +2,9 @@ package wumpusworld;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
@@ -12,11 +14,23 @@ import javax.swing.border.EmptyBorder;
 
 public class GameWindow {
 
-    private JButton[][] buttonGrid ;
-    
+    private JButton[][] buttonGrid;
+    private ImageIcon kevinImg;
+    private ImageIcon fireImg;
+    private ImageIcon goldImg;
+    private ImageIcon groundImg;
+    private ImageIcon kevinInFireImg;
+    private ImageIcon kevinInGoldImg;
+    private ImageIcon kevinInMonsterImg;
+    private ImageIcon monsterImg;
+    private ImageIcon rockImg;
+    private int windowSize;
+    private Board currentBoard;
+
     public GameWindow(Board mBoard) {
-        int windowSize = 800;
+        windowSize = 800;
         buttonGrid = new JButton[mBoard.col][mBoard.row];
+        currentBoard = mBoard;
         JFrame window = new JFrame();
         window.setTitle("Le monde de Wumpus");
         window.setSize(1200, windowSize);
@@ -27,108 +41,124 @@ public class GameWindow {
         //Generate window info
         JPanel configPanel = new JPanel();
         configPanel.setLayout(new BoxLayout(configPanel, BoxLayout.PAGE_AXIS));
-        
+
         // Label Generate Paths
         JPanel generateLabelPanel = new JPanel();
         generateLabelPanel.setLayout(new BoxLayout(generateLabelPanel, BoxLayout.LINE_AXIS));
         JLabel generateLabel = new JLabel("Generate Paths: ");
-        generateLabel.setBorder(new EmptyBorder(10,10,10,10));
+        generateLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
         generateLabelPanel.add(generateLabel);
-        
+
         // Simple generate mode
         JPanel simpleModPanel = new JPanel();
         simpleModPanel.setLayout(new BoxLayout(simpleModPanel, BoxLayout.LINE_AXIS));
         simpleModPanel.add(new JButton("Simple Mod"));
         JLabel simpleModTimeLabel = new JLabel("Time");
-        simpleModPanel.setBorder(new EmptyBorder(10,10,10,10));
-        simpleModTimeLabel.setBorder(new EmptyBorder(10,10,10,10));
+        simpleModPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        simpleModTimeLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
         simpleModPanel.add(simpleModTimeLabel);
-        
+
         // Djikstra generate mode
         JPanel djikstraModPanel = new JPanel();
         djikstraModPanel.setLayout(new BoxLayout(djikstraModPanel, BoxLayout.LINE_AXIS));
         djikstraModPanel.add(new JButton("Djikstra Mod"));
         JLabel djikstraModTimeLabel = new JLabel("Time");
-        djikstraModTimeLabel.setBorder(new EmptyBorder(10,10,10,10));
-        djikstraModPanel.setBorder(new EmptyBorder(10,10,10,10));
+        djikstraModTimeLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
+        djikstraModPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         djikstraModPanel.add(djikstraModTimeLabel);
-        
+
         // Label Move Kevin 
         JPanel moveKevinLabelPanel = new JPanel();
         moveKevinLabelPanel.setLayout(new BoxLayout(moveKevinLabelPanel, BoxLayout.LINE_AXIS));
         JLabel moveKevinLabel = new JLabel("Move Kevin: ");
-        moveKevinLabel.setBorder(new EmptyBorder(10,10,10,10));
+        moveKevinLabel.setBorder(new EmptyBorder(10, 10, 10, 10));
         moveKevinLabelPanel.add(moveKevinLabel);
-        
+
         // Checkbox level mode
         JPanel moveKevinPanel = new JPanel();
         moveKevinPanel.setLayout(new BoxLayout(moveKevinPanel, BoxLayout.PAGE_AXIS));
-        
+
         JPanel yoloPanel = new JPanel();
         yoloPanel.setLayout(new BoxLayout(yoloPanel, BoxLayout.LINE_AXIS));
         yoloPanel.add(new JCheckBox("YOLO"));
-        yoloPanel.setBorder(new EmptyBorder(10,10,10,10));
+        yoloPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         JPanel commonPanel = new JPanel();
         commonPanel.setLayout(new BoxLayout(commonPanel, BoxLayout.LINE_AXIS));
         commonPanel.add(new JCheckBox("Commom decision"));
-        commonPanel.setBorder(new EmptyBorder(10,10,10,10));
+        commonPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         JPanel mamaPanel = new JPanel();
         mamaPanel.setLayout(new BoxLayout(mamaPanel, BoxLayout.LINE_AXIS));
         mamaPanel.add(new JCheckBox("Mama's son"));
-        mamaPanel.setBorder(new EmptyBorder(10,10,10,10));
+        mamaPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         JPanel manualPanel = new JPanel();
         manualPanel.setLayout(new BoxLayout(manualPanel, BoxLayout.LINE_AXIS));
         manualPanel.add(new JButton("Manual"));
-        manualPanel.setBorder(new EmptyBorder(10,10,10,10));
+        manualPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
         JPanel autoPanel = new JPanel();
         autoPanel.setLayout(new BoxLayout(autoPanel, BoxLayout.LINE_AXIS));
-        autoPanel.add(new JButton("Auto"));
-        autoPanel.setBorder(new EmptyBorder(10,10,10,10));
-        
+        JButton autoButton = new JButton("Auto");
+        autoPanel.add(autoButton);
+        autoPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
         moveKevinPanel.add(yoloPanel);
         moveKevinPanel.add(commonPanel);
         moveKevinPanel.add(mamaPanel);
         moveKevinPanel.add(manualPanel);
         moveKevinPanel.add(autoPanel);
-        
+
         // add elements to configPanel
         configPanel.add(generateLabelPanel);
         configPanel.add(simpleModPanel);
         configPanel.add(djikstraModPanel);
         configPanel.add(moveKevinLabelPanel);
         configPanel.add(moveKevinPanel);
-        
+
         // add both JPanel to window
         window.getContentPane().add(gridPanel, BorderLayout.CENTER);
         window.getContentPane().add(configPanel, BorderLayout.EAST);
-        
+
+        // images
+        fireImg = new ImageIcon("images/fire.jpg");
+        goldImg = new ImageIcon("images/gold.jpg");
+        groundImg = new ImageIcon("images/ground.jpg");
+        kevinImg = new ImageIcon("images/kevin.jpg");
+        kevinInFireImg = new ImageIcon("images/kevininfire.jpg");
+        kevinInGoldImg = new ImageIcon("images/keviningold.jpg");
+        kevinInMonsterImg = new ImageIcon("images/kevininmonster.jpg");
+        monsterImg = new ImageIcon("images/monster.jpg");
+        rockImg = new ImageIcon("images/rock.jpg");
+
         // start filling grid
         for (int row = 0; row < buttonGrid.length; row++) {
             for (int col = 0; col < buttonGrid[row].length; col++) {
                 String cellLabel = "";
+                ImageIcon image = groundImg;
                 if (mBoard.getBoard()[row][col].getWall() == true) {
-                    cellLabel += "WALL \n";
+                    image = rockImg;
                 }
-                if (mBoard.getBoard()[row][col].getHole() == true) {
-                    cellLabel += "HOLE \n";
+                if (mBoard.getBoard()[row][col].getHole() == true) {       
+                    image = fireImg;
                 }
                 if (mBoard.getBoard()[row][col].getMonster() == true) {
-                    cellLabel += "MONSTER \n";
+                   image = monsterImg;
                 }
                 if (mBoard.getBoard()[row][col].getPlayer() == true) {
-                    cellLabel += "KEVIN \n";
+                    image = kevinImg;
                 }
                 if (mBoard.getBoard()[row][col].getGold() == true) {
-                    cellLabel += "GOLD \n";
+                    image = goldImg;
                 }
                 if (mBoard.getBoard()[row][col].getSmell() == true) {
-                    cellLabel += "SMELL \n";
+                    
                 }
                 if (mBoard.getBoard()[row][col].getWind() == true) {
-                    cellLabel += "WIND \n";
+                    
                 }
-                buttonGrid[row][col] = new JButton(cellLabel);
+                buttonGrid[row][col] = new JButton();
                 buttonGrid[row][col].setEnabled(false);
+                image.getImage().getScaledInstance( windowSize/currentBoard.col, windowSize/currentBoard.col, Image.SCALE_DEFAULT);
+                buttonGrid[row][col].setIcon(image);
+                buttonGrid[row][col].setDisabledIcon(image);
                 gridPanel.add(buttonGrid[row][col]);
             }
         }
@@ -136,35 +166,38 @@ public class GameWindow {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setVisible(true);
     }
-    
-    
-    public void refreshBoard(Board board){
-         // start filling grid
+
+    public void refreshBoard(Board board) {
+        // start filling grid
         for (int row = 0; row < buttonGrid.length; row++) {
             for (int col = 0; col < buttonGrid[row].length; col++) {
-                String cellLabel = "";
+
+                ImageIcon image = groundImg;
+
                 if (board.getBoard()[row][col].getPlayer() == true) {
-                    cellLabel += "KEVIN \n";
+                    image = kevinImg;
                 }
-                if (board.getBoard()[row][col].getWall() == true) {
-                    cellLabel += "WALL \n";
+                else if (board.getBoard()[row][col].getWall() == true) {
+                    image = rockImg;
                 }
-                if (board.getBoard()[row][col].getHole() == true) {
-                    cellLabel += "HOLE \n";
+                else if (board.getBoard()[row][col].getHole() == true) {
+                    image = fireImg;
                 }
-                if (board.getBoard()[row][col].getMonster() == true) {
-                    cellLabel += "MONSTER \n";
+                else if (board.getBoard()[row][col].getMonster() == true) {
+                    image = monsterImg;
                 }
-                if (board.getBoard()[row][col].getGold() == true) {
-                    cellLabel += "GOLD \n";
+                else if (board.getBoard()[row][col].getGold() == true) {
+                    image = goldImg;
                 }
-                if (board.getBoard()[row][col].getSmell() == true) {
-                    cellLabel += "SMELL \n";
+                else if (board.getBoard()[row][col].getSmell() == true) {
+                 
                 }
-                if (board.getBoard()[row][col].getWind() == true) {
-                    cellLabel += "WIND \n";
-                }
-                buttonGrid[row][col].setText(cellLabel);
+                else if (board.getBoard()[row][col].getWind() == true) {
+                   
+                } 
+                image.getImage().getScaledInstance( windowSize/currentBoard.col, windowSize/currentBoard.col, Image.SCALE_DEFAULT);
+                buttonGrid[row][col].setIcon(image);
+                buttonGrid[row][col].setDisabledIcon(image);
             }
         }
     }
